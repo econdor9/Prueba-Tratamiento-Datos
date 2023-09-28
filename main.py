2f5f4187131847d88efdaa3922ada5bff0c308e6
econdor9 <edisoncondor9@gmail.com>
2023-09-28 09:02:09 -0500

    Extracción de todos los títulos.

--- main.py
+++ main.py
@@ -17,9 +17,18 @@ try:
     # Encontrar los titulos
     title_elements = driver.find_elements(By.CSS_SELECTOR, "div[id^='title_']")
 
-    # mostrar todos los títulos que se cargan en el sítio web
-    for title_element in title_elements:
-        print(title_element.text)
+    # Encontrar las distancias
+    distance_elements = driver.find_elements(By.CSS_SELECTOR, "div[data-testid='listing-card-subtitle'] span span")
+
+    # Mostrar títulos y distancias
+    for title_element, distance_element in zip(title_elements, distance_elements):
+        title = title_element.text
+        distance = distance_element.text
+
+        # Imprimir títulos y distancias
+        print("Título:", title)
+        print("Distancia:", distance)
+        print("=" * 40)
 
 except Exception as e:
     print("Error:", str(e))
